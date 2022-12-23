69c501f3c6b0ea69a6fd1296bc9425a7c917a24f
--replace-all <zinovskayak@gmail.com>
2022-12-23 21:11:54 +0100

    решение задачи 11 через метод

--- 08_task11/Program.cs
+++ 08_task11/Program.cs
@@ -4,12 +4,25 @@
 // 456 -> 46
 // 782 -> 72
 // 918 -> 98
-int number = new Random().Next(100, 1000);
-Console.WriteLine($"Случайное трехзначное число => {number}");
 
-int firstDigit = number / 100;
-int lastDigit = number % 10;
+// int number = new Random().Next(100, 1000);
+// Console.WriteLine($"Случайное трехзначное число => {number}");
 
-int result = firstDigit * 10 + lastDigit;
+// int firstDigit = number / 100;
+// int lastDigit = number % 10;
 
-Console.WriteLine(result);
+// int result = firstDigit * 10 + lastDigit;
+
+// Console.WriteLine(result);
+
+// решение с помощью метода
+
+int rnd = new Random().Next(100, 1000);
+int MakeDigit(int number)
+{
+    int result = (number/100)*10 + number % 10;
+    return result;
+}
+
+int newDigit = MakeDigit(rnd);
+Console.WriteLine($"{rnd} -> {newDigit}");
