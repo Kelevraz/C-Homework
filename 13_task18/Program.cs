3114d2a8c67ebbac2e4cd25befb6ed2a59f05b00
--replace-all <zinovskayak@gmail.com>
2022-12-28 15:08:24 +0100

    решена задача 18 - определение координат по четверти

---
+++ 13_task18/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти,
+// показывает диапазон возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string answer = Quarter(number);
+
+Console.WriteLine(answer);
+
+string Quarter(int num)
+{
+    if(num == 1) return "xc > 0, yc > 0";
+    if(num == 2) return "xc < 0, yc > 0";
+    if(num == 3) return "xc < 0, yc < 0";
+    if(num == 4) return "xc > 0, yc < 0";
+    return "такой четверти не существует";
+}
