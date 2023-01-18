8263ad7e091146470763ef98ba48c26eb4523c9a
--replace-all <zinovskayak@gmail.com>
2023-01-18 21:16:23 +0100

    доп решение задачи 30

--- 19_task30/Program.cs
+++ 19_task30/Program.cs
@@ -22,3 +22,26 @@ Console.Write($"{collection[i]} ");
 int[] array = new int[8]; // 0, 1, 2, 3, 4, 5, 6, 7
 FillArray(array);
 PrintArray(array);
+
+// вариант 2
+
+// int[] CreateArray(int size)
+// {
+// int[] array = new int[size];
+// Random rnd = new Random();
+// for (int i = 0; i < array.Length; i++)
+// {
+// array[i] = rnd.Next(0, 2);
+// }
+// return array;
+// }
+
+// void PrintArray(int[] arr)
+// {
+// for (int i = 0; i < arr.Length; i++)
+// {
+// Console.Write($"{arr[i]} ");
+// }
+// }
+// int[] arr1 = CreateArray(8);
+// PrintArray(arr1);
